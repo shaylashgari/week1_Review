0c8389cc3a36e0ad95b0f109b2f9eeee99f9e3a1
Shay <shaylashgari@gmail.com>
2024-09-30 10:38:26 -0500

    first commit

---
+++ src/main/java/com/ps/Calculator.java
@@ -0,0 +1,26 @@
+package com.ps;
+class Calculator {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    // Static add method
+    // Static subtract method
+    // Static multiplication method
+    // Static division method
+
+    // recieve two inputs (Ints)
+    // Return a single number (int)
+
+    // Extra features:
+    // Extras
+    // Static modulo method
+    // Three inputs
+    // Calculate area
+    // Calculate perimeter
+
+}
+
+
